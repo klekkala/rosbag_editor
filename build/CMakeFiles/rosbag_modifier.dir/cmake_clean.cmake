06062b6098dc035e7f37c2a8f13049104c4fd473
kiran4399 <kiran4399@gmail.com>
2016-06-19 10:10:35 +0530

    image write to bag done

---
+++ build/CMakeFiles/rosbag_modifier.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/rosbag_modifier.dir/main.cc.o"
+  "CMakeFiles/rosbag_modifier.dir/Thirdparty/DLib/FileFunctions.cpp.o"
+  "../bin/rosbag_modifier.pdb"
+  "../bin/rosbag_modifier"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/rosbag_modifier.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
